96585cf5aed36fffb34ce525c8d32207cb550c38
Christian Rivasseau <chrivasseau@google.com>
2013-05-09 20:56:48 -0400

    Going up to euler 6

--- euler.cpp
+++ euler.cpp
@@ -53,28 +53,61 @@ uint64_t GetLargestPrime() {
   return largest_prime_divider;
 }
 
-bool IsPalindrome(int n) {
-  vector<int> d;
-  int decimal = 10;
-  for () {
+vector<int> Decompose(int n) {
+  vector<int> result;
+  int k = n;
+  while (k > 0) {
+    int value = k % 10;
+    result.push_back(value);
+    k = k - value;
+    k = k / 10;
   }
+  return result;
 }
 
-int LargestPalindrome() {
-  int largest_prime_divider = 2;
-  for (uint64_t i = 2; i <= 775147; ++i) {
-    if (600851475143 % i == 0 && IsPrime(i)) {
-      largest_prime_divider = i;
+bool IsPalindrome(int n) {
+  vector<int> decomposed = Decompose(n);
+  int size = decomposed.size();
+  for (int i = 0; i < size / 2; ++i) {
+    if (decomposed[i] != decomposed[size - 1 - i]) {
+      return false;
     }
   }
-  return largest_prime_divider;
+  return true;
+}
+
+int LargestPalindrome(int max) {
+  int largest_palindrome = 0;
+  for (int i = 0; i < max; ++i) {
+    for (int j = 0; j < max; ++j) {
+      int product = i * j;
+      if (IsPalindrome(product) && product > largest_palindrome) {
+        largest_palindrome = product;
+      }
+    }
+  }
+  return largest_palindrome;
+}
+
+uint64_t SumSquareDifference(int bound) {
+  uint64_t result = 0;
+  for (int i = 1; i <= bound; ++i) {
+    for (int j = 1; j <= bound; ++j) {
+      if (j != i) {
+        result += i * j;
+      }
+	}
+  }
+  return result;
 }
 }  // anonymous namespace
 
 int main(int argc, char* argv[]) {
-  printf("Euler 1: %d\n", GetSum());
-  printf("Euler 2: %d\n", GetFibonacci());
-  printf("Euler 2: %ld\n", GetLargestPrime());
+ // printf("Euler 1: %d\n", GetSum());
+ // printf("Euler 2: %d\n", GetFibonacci());
+ // printf("Euler 3: %ld\n", GetLargestPrime());
+ // printf("Euler 4: %d\n", LargestPalindrome(1000));
+  printf("Euler 6: %ld\n", SumSquareDifference(100));
   return 0;
 }
 
